6573ccb999df3acdfa559e6c9eca095febaec336
it-ly <itxiaobai_ly@yeah.net>
2019-09-07 15:20:50 +0800

    登录验证码

--- Shop/src/main/java/com/nt/controller/UserController.java
+++ Shop/src/main/java/com/nt/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.nt.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.nt.pojo.User;
 import com.nt.service.UserService;
+import com.nt.util.RanCode;
 @Controller
 @RequestMapping("user")
 public class UserController {
@@ -20,11 +25,40 @@ public class UserController {
         ModelAndView mav = new ModelAndView("before/login");
 		return mav;
     }
+    /**
+	 * 获取生成验证码显示到 UI 界面
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@RequestMapping("checkCode")
+	public void checkCode(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		//设置相应类型,告诉浏览器输出的内容为图片
+        response.setContentType("image/jpeg");
+        //设置响应头信息，告诉浏览器不要缓存此内容
+        response.setHeader("pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expire", 0);
+        RanCode randomValidateCode = new RanCode();
+        try {
+            randomValidateCode.getRandcode(request, response);//输出图片方法
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 	@RequestMapping("userLogin")
-    public ModelAndView userLogin(User user,HttpServletRequest request){
+    public ModelAndView userLogin(User user,String code,HttpServletRequest request){
 		HttpSession session = request.getSession();
-		User us=userService.login(user);
 		ModelAndView mav = new ModelAndView();
+		String verifyCode= (String) session.getAttribute("randomcode_key");
+        if(!code.equalsIgnoreCase(verifyCode)){
+        	mav.setViewName("before/login");
+        	mav.addObject("msg","验证码错误！");
+        	return mav;
+        }
+		User us=userService.login(user);
 		if(us!=null) {
 			int id=us.getUserid();
 			mav.setViewName("before/index");
